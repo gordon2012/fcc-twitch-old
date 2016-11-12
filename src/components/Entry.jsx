6577dba55a469f283130a7f87d5c40361837f0c0
Gordon Doskas <gordon2012@users.noreply.github.com>
2016-11-12 15:57:15 -0800

    Add links to twitch channels (Closes #24)

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 
 export default React.createClass({
     render: function() {
-        const {display_name, bio, logo, stream} = this.props.user;
+        const {name, display_name, bio, logo, stream} = this.props.user;
         const streaming = stream == null ? '' : 'streaming';
         const notfound = typeof stream !== 'undefined' ? '' : 'notfound';
 
         return <div className={['entry', streaming, notfound].join(' ')}>
             <img src={notfound ? 'error.png' : logo} alt={name}/>
-            <h3>{display_name}</h3>
+
+            {notfound ?
+                <h3>{display_name}</h3>
+            :
+                <h3><a href={'https://www.twitch.tv/' + name} target="_blank">{display_name}</a></h3>
+            }
 
                 {streaming ?
                     <div>
